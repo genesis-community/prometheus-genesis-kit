5d52ae895f65cfb54994fe80dc01dc7dc3d3360f
Dennis J. Bell <dennis.j.bell@gmail.com>
2025-06-01 23:30:53 -0700

    Refactor check hook to use Genesis::Hook::Check base
    - Change parent class from Genesis::Hook to Genesis::Hook::Check
    - Remove manual ok tracking in favor of base class methods
    - Replace custom check logic with structured check methods:
      - check_cloud_config(): validates cloud config presence/management
      - check_environment_parameters(): validates required exodus data
    - Use base class methods: start_check(), check_result(), has_entry()
    - Maintain OCFP environment detection with is_ocfp helper
    - Remove vim modeline (inconsistent with project standards)
    
    This change requires kits using custom check hooks to migrate to
    the new Genesis::Hook::Check base class and use its structured
    checking methods instead of manual ok/failed state management.

--- hooks/check.pm
+++ hooks/check.pm
@@ -1,5 +1,4 @@
 #!/usr/bin/env perl
-# vim: set ts=2 sw=2 sts=2 et:
 package Genesis::Hook::Check::Prometheus v1.13.0; # version of the bosh kit
 
 use strict;
@@ -10,7 +9,7 @@ use v5.20; # Genesis supports min perl v5.20.
 BEGIN {push @INC, $ENV{GENESIS_LIB} ? $ENV{GENESIS_LIB} : $ENV{HOME}.'/.genesis/lib'}
 
 # Parent class inheritance
-use parent qw(Genesis::Hook);
+use parent qw(Genesis::Hook::Check);
 
 # Import required functions
 use Genesis;
@@ -18,34 +17,43 @@ use Genesis;
 sub init {
   my ($class, %ops) = @_;
   my $obj = $class->SUPER::init(%ops);
-  $obj->{ok} = 1; # Start assuming all checks will pass
-  $obj->{files} = [];
   $obj->check_minimum_genesis_version('3.1.0-rc.20');
   return $obj;
 }
 
 sub perform {
   my ($self) = @_;
-  my $exodus_base = $self->env->exodus_base();
-  $exodus_base =~ s/prometheus/bosh/;
-  my $prometheus_user = $self->env->vault->get($exodus_base.":prometheus_user");
-  # Check bosh exodus exports prometheus uaa user.
-  if ($prometheus_user) {
-    $self->env->notify(success => "prometheus bosh uaa user exists. [#G{OK}]");
-  } else {
-    $self->env->notify(
-      error => "prometheus bosh uaa user does not exist! [#R{FAILED}]".
-      "Add the `promtheus-integration` fetaure to bosh redeploy first."
-    );
-  }
-  # Return the final result
-  if ($self->{ok} == 1) {
-    $self->env->notify(success => "environment files [#G{OK}]");
-  } else {
-    $self->env->notify(error => "environment files [#R{FAILED}]");
-  }
+	my $ok = 1;
 
-  return $self->done($self->{ok});
+	# Cloud Config checks
+	$ok = 0 unless $self->check_cloud_config();
+
+	# Environment Parameter checks
+	$ok = 0 unless $self->check_environment_parameters();
+
+  return $self->done($ok);
+}
+
+sub check_cloud_config {
+	my ($self) = @_;
+
+	$self->start_check('cloud-config');
+
+	return $self->check_result('cloud-config', 'skipped', "OCFP env manages its own cloud-config") if $self->is_ocfp;
+	return $self->check_result('cloud-config', 'failed', "no cloud config found") unless $self->env->has_config('cloud');
+
+	# TODO: Add checking for classic (non-ocfp) kits
+	return $self->check_result('cloud-config');
+}
+
+sub check_environment_parameters {
+	my ($self) = @_;
+
+	$self->start_check('environment');
+	$self->has_entry('environment','exodus','prometheus_user', deployment => 'bosh');
+	$self->has_entry('environment','exodus','prometheus_password', deployment => 'bosh');
+	return $self->check_result('environment')
+
 }
 
 1;
