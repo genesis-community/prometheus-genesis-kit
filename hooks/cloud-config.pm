84a360d23009b85c023a968f1f499ec36a1414cd
Ioannis Tsouvalas <tsouvalasi@atic.gr>
2026-06-17 15:17:35 +0000

    Update for release v2.1.0
    Updated kit version and perl hook packages

--- hooks/cloud-config.pm
+++ hooks/cloud-config.pm
@@ -1,4 +1,4 @@
-package Genesis::Hook::CloudConfig::Prometheus v2.0.0;
+package Genesis::Hook::CloudConfig::Prometheus v2.1.0;
 
 use v5.20;
 use warnings;
--- hooks/info.pm
+++ hooks/info.pm
@@ -1,4 +1,4 @@
-package Genesis::Hook::Info::Prometheus v2.0.0;
+package Genesis::Hook::Info::Prometheus v2.1.0;
 
 use v5.20; # Genesis min perl version is 5.20
 use warnings;
