503c6587c3dc8e1e99ead5390c2708cd0bd1b557
Wayne E. Seguin <wayneeseguin@gmail.com>
2026-07-16 11:45:26 -0400

    Add Proxmox VE CPI support to cloud config
    Add pve cloud properties for networks (bridge), vm_types
    (cpu, ram, disk, network_bridge), and disk_types (storage,
    disk_format), all overridable via bosh-configs.cpi params.
    Declare pve in kit.yml supported CPIs.

--- hooks/cloud-config.pm
+++ hooks/cloud-config.pm
@@ -44,6 +44,9 @@ sub perform {
               aws => {
                 'subnet' => $self->subnet_reference('id'),
               },
+              pve => {
+                'bridge' => scalar($self->env->lookup('bosh-configs.cpi.pve_network_bridge', 'lvnet001')),
+              },
             },
           },
         )
@@ -96,6 +99,12 @@ sub perform {
                 'http_tokens' => 'required',
               },
             },
+            pve => {
+              'cpu'            => scalar($self->env->lookup('bosh-configs.cpi.pve_prometheus_cpu',  $self->for_scale({ dev => 2, prod => 4 }, 2))),
+              'ram'            => scalar($self->env->lookup('bosh-configs.cpi.pve_prometheus_ram',  $self->for_scale({ dev => 4096, prod => 8192 }, 4096))),
+              'disk'           => scalar($self->env->lookup('bosh-configs.cpi.pve_prometheus_disk', $self->for_scale({ dev => 32768, prod => 65536 }, 32768))),
+              'network_bridge' => scalar($self->env->lookup('bosh-configs.cpi.pve_network_bridge', 'lvnet001')),
+            },
           },
         ),
       ],
@@ -117,6 +126,10 @@ sub perform {
             aws => {
               'type' => 'gp3',
               'encrypted' => $self->TRUE,
+            },
+            pve => {
+              'storage'     => scalar($self->env->lookup('bosh-configs.cpi.pve_disk_storage', 'zfs-1')),
+              'disk_format' => scalar($self->env->lookup('bosh-configs.cpi.pve_disk_format', 'raw')),
             },
           },
         ),
